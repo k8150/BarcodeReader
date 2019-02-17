32a67f4a894d89941c0ec3d3dbb15bd55d988494
k.haigo <kojihaigo@gmail.com>
2019-02-17 13:29:15 +0900

    initial commit

---
+++ SwiftBarcodeReaderProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftBarcodeReaderProj
+//
+//  Created by haigo koji on 2019/02/17.
+//  Copyright © 2019 haigo koji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
